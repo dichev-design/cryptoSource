ff524183c3c7fec7fcf706bbfb7c6e4e5b34e71e
dichev-design <dichev.official@gmail.com>
2026-02-13 11:50:12 +0100

    Testing name change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <h1>You Add One</h1>
+            <h1>cryptoSource</h1>
             <div>
                 <Link to="/">Home</Link>
                 <Link to="/login">Login</Link>
